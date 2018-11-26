16ff684f5659fda7a1ec0ce504919c4cdf90c86a
Johan Cederschiold <johan.cederschiold@gmail.com>
2018-11-26 20:38:38 +0100

    created experimental class for alterantive UI

---
+++ src/questions/UserInterfaceWIP.java
@@ -0,0 +1,16 @@
+package questions;
+
+import javax.swing.JFrame;
+
+public class UserInterfaceWIP extends JFrame {
+	
+	/* 	This is an experimental Class under the branch alternativeUI
+	 * 
+	 */
+	
+	
+	
+	
+	
+
+}
